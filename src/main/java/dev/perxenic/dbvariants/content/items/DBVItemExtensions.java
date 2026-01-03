6ebaeda0b78f7978a97eee03de5b8dee4bc72010
Auseawesome <70506988+Auseawesome@users.noreply.github.com>
2026-01-03 22:29:32 +0000

    Documented the item extensions

--- src/main/java/dev/perxenic/dbvariants/content/items/DBVItemExtensions.java
+++ src/main/java/dev/perxenic/dbvariants/content/items/DBVItemExtensions.java
@@ -4,9 +4,20 @@ import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
 import net.neoforged.neoforge.client.extensions.common.IClientItemExtensions;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * Item extensions class to set custom renderer for items
+ * @see DBVItemRenderer
+ */
 public class DBVItemExtensions implements IClientItemExtensions {
+    /**
+     * The {@link DBVItemRenderer} instance
+     */
     private final DBVItemRenderer itemRenderer = new DBVItemRenderer();
 
+    /**
+     * Returns the custom renderer, used by Minecraft to render items
+     * @return The {@link DBVItemRenderer} instance as a {@link BlockEntityWithoutLevelRenderer}
+     */
     @Override
     public @NotNull BlockEntityWithoutLevelRenderer getCustomRenderer() {
         return itemRenderer;
